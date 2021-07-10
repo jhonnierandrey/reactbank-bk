0b3b95a81f164ec277cb85a9100e0ff9df0a85ec
jhonnierandrey <jhonnierandrey@outlook.com>
2021-07-10 19:39:06 -0300

    reqs updated

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 # Ensure responses aren't cached
-# @app.after_request
-# def after_request(response):
-#     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     response.headers["Expires"] = 0
-#     response.headers["Pragma"] = "no-cache"
-#     # response.headers["Access-Control-Allow-Origin"] = "*"
-#     return response
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    # response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
 
 
 # Custom filter
