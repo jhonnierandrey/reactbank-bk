ced9ffb7181768bbd75064252c5901739869658b
jhonnierandrey <jhonnierandrey@outlook.com>
2021-07-11 00:09:31 -0300

    reqs updated

--- app.py
+++ app.py
@@ -28,7 +28,11 @@ def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
-    # response.headers["Access-Control-Allow-Origin"] = "*"
+    # response.headers["Access-Control-Allow-Credentials"] = 'true'
+    # response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+    # response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+    # response.headers["Access-Control-Allow-Origin"] = '*'
+    
     return response
 
 
